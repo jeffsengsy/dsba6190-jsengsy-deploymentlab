82a1e677d304e1ecd7d85e5f0bcde01933bc77d3
jeffsengsy <jsengsy@outlook.com>
2025-11-19 19:53:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -72,10 +72,10 @@ resource "azurerm_storage_account" "storage" {
 // SQL Server
 
 resource "azurerm_sql_server" "sql" {
-  name                         = "sql-${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
+  name                = "sql-${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  version             = "12.0"
 
   administrator_login          = var.sql_admin_username
   administrator_login_password = var.sql_admin_password
@@ -91,7 +91,7 @@ resource "azurerm_sql_database" "db" {
   location            = azurerm_resource_group.rg.location
   server_name         = azurerm_sql_server.sql.name
 
-  sku_name    = "Basic" 
+  sku_name    = "Basic"
   max_size_gb = 2
 
   tags = local.tags
