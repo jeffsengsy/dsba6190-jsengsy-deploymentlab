68c4ed5a3879d7f69c550cb067cabcec0f7db3e7
jeffsengsy <jsengsy@outlook.com>
2025-11-19 20:37:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -81,8 +81,8 @@ resource "azurerm_mssql_server" "sql" {
   location            = azurerm_resource_group.rg.location
 
   version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
   minimum_tls_version          = "1.2"
 
   tags = local.tags
